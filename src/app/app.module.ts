5c3a230cb66ed5fe1041b0d50ac8104fe1c52906
Marc Helmisaari <marchelmisaari@host-78-64-10-145.homerun.telia.com>
2017-10-11 10:57:55 +0200

    Added bland page for commenting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FormPage } from '../pages/formPage/formPage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FormPage,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FormPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/formPage/formPage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-form',
+  templateUrl: 'formPage.html'
+})
+export class FormPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
