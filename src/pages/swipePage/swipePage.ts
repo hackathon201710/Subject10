b89b8320965e9cfb9a6f2d12d98c2959ae762370
Marc Helmisaari <marchelmisaari@host-78-64-10-145.homerun.telia.com>
2017-10-11 14:25:13 +0200

    Updated version

--- src/pages/swipePage/swipePage.ts
+++ src/pages/swipePage/swipePage.ts
@@ -3,6 +3,8 @@ import { NavController } from 'ionic-angular';
 import { PictureService } from "../../service/PictureService";
 import { Http } from '@angular/http';
 import 'rxjs/Rx';
+import {FormPage} from "../formPage/formPage";
+
 
 
 import {
@@ -25,8 +27,11 @@ export class SwipePage {
   @ViewChild('myswing1') swingStack: SwingStackComponent;
   @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
 
+
+  cnt: number = 0;
+  currentIndex: number = 0;
   listOfContacts: any[];
-  profile: sting;
+  profile: string;
   cards: Array<any>;
   stackConfig: StackConfig;
   recentCard: string = '';
@@ -36,7 +41,7 @@ export class SwipePage {
    //[{"gender":"female","name":{"title":"ms","first":"scarlett","last":"bradley"},"location":{"street":"9981 boghall road","city":"ratoath","state":"cavan","postcode":11268},"email":"scarlett.bradley@example.com","login":{"username":"goldenpeacock277","password":"passpass","salt":"UUNSX7Uo","md5":"b275f9ae9140a42e3de800916bcefc91","sha1":"227f1f89c0ffd46464be9a7669a0083a7dcdbb4a","sha256":"50b4ba317a33b4c3c4e5fe9287e454a66d0a6e0880da5ea4fc47518208cba91d"},"dob":"1968-09-01 04:41:42","registered":"2007-01-25 07:06:51","phone":"071-299-4616","cell":"081-578-2624","id":{"name":"PPS","value":"8170144T"},"picture":{"large":"https://randomuser.me/api/portraits/women/47.jpg","medium":"https://randomuser.me/api/portraits/med/women/47.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/47.jpg"},"nat":"IE"}]
 
 
-  constructor(public pictureService: PictureService) {
+  constructor(public navCtrl: NavController, public pictureService: PictureService) {
     this.stackConfig = {
       throwOutConfidence: (offsetX, offsetY, element) => {
         return Math.min(Math.abs(offsetX) / (element.offsetWidth/2), 1);
@@ -50,7 +55,6 @@ export class SwipePage {
     };
 
     this.listOfContacts = this.pictureService.getPictureData();
-    this.profile = this.pictureService.getPictureData()[1];
     
     console.log("This is the displaying profile", this.profile)
     console.log("This is a list of contacts: ", this.listOfContacts);
@@ -90,20 +94,29 @@ voteUp(like: boolean) {
   //this.addNewCards(1);
   if (like) {
     this.recentCard = 'You liked: ' + removedCard.email;
+    this.cnt++;
+    console.log(removedCard)
   } else {
+    this.navCtrl.push(FormPage, {cnt: this.cnt});
     this.recentCard = 'You disliked: ' + removedCard.email;
+    this.cnt++;
   }
 }
  
 // Add new cards to our array
-addNewCards(count: number) {
-
-  this.cards.push(this.profile)
-  this.cards.push(this.listOfContacts[0])
-  this.cards.push(this.listOfContacts[1])
-  this.cards.push(this.listOfContacts[2])
-  this.cards.push(this.listOfContacts[3])
+addNewCards(count: number) { 
+  this.cards.push(this.listOfContacts[5])
   this.cards.push(this.listOfContacts[4])
+  this.cards.push(this.listOfContacts[3])
+  this.cards.push(this.listOfContacts[2])
+  this.cards.push(this.listOfContacts[1])
+  this.cards.push(this.listOfContacts[0])
+
+ 
+  
+  
+  
+  
 }
  
 // http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
