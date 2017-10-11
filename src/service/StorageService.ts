5a0c7e7ceb60f7d7e8b88d4de8716d95ef0bed13
markbassant <marcus.bassant@gmail.com>
2017-10-11 13:36:26 +0200

    Serveral

--- src/service/StorageService.ts
+++ src/service/StorageService.ts
@@ -1,12 +1,45 @@
 import {Injectable} from "@angular/core";
 import {Picture} from "../model/Picture";
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class StorageService {
 
-  savePictureData(picture: Picture) {
-    if (!picture) {
+  constructor(private storage: Storage) {
+  }
+
+  getPictureData(): Promise<Picture[]> {
+    return new Promise((resolve, reject) => {
+      this.storage.get("pictures").then((val) => {
+        let result = <Picture[]>JSON.parse(val);
+        resolve(result);
+      });
+    });
+  }
+
+  savePictureData(picture: Picture): Promise<any> {
+    if (!picture || !picture.name) {
       return;
     }
+    return new Promise((resolve, reject) => {
+      this.getPictureData().then((data: Picture[]) => {
+        let saved = false;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].name === picture.name) {
+            data[i] = picture;
+            saved = true;
+          }
+          if (!saved) {
+            data.push(picture);
+          }
+          this.storage.set("pictures", data);
+          resolve();
+        }
+      });
+    });
+  }
+
+  deletePictures() {
+    this.storage.set("pictures", []);
   }
 }
